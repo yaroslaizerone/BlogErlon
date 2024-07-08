dc1793ec55bc86ed3eb5224c676194ad90224772
ярослав Колпаков <kolpackov.yarosl@yandex.ru>
2024-07-08 15:23:11 +0700

    Add Tag and Post in AdminPanel

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -37,7 +37,7 @@
                                             Заголовок поста</label>
                                         <input type="text" name="title" class="form-control" id="exampleInputEmail1"
                                                aria-describedby="emailHelp"
-                                        value="{{ old('title') }}">
+                                               value="{{ old('title') }}">
                                         @error('title')
                                         <div class="text-danger"> Это поле должно быть заполнено</div>
                                         @enderror
@@ -49,10 +49,34 @@
                                         <textarea id="summernote" name="content">
                                             {{ old('content') }}
                                         </textarea>
-                                         @error('content')
+                                        @error('content')
                                         <div class="text-danger"> Это поле должно быть заполнено</div>
                                         @enderror
                                     </form>
+                                    <div class="form-group w-50">
+                                        <label for="exampleInputFile">Добавить превью</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" class="custom-file-input" name="preview_image">
+                                                <label class="custom-file-label">Выберите изображение</label>
+                                            </div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text">Загрузка</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group w-50">
+                                        <label for="exampleInputFile">Добавить главное изображение</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" class="custom-file-input" name="main_image">
+                                                <label class="custom-file-label">Выберите изображение</label>
+                                            </div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text">Загрузка</span>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="card-footer">
                                         <button type="submit" class="btn btn-primary">Добавить пост</button>
                                     </div> <!--end::Footer-->
